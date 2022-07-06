7ff4ccb1b1fa6bdb5508a1a6072de8ed28a636ef
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-07-06 13:12:12 +0300

    add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,13 @@ import React from 'react';
 import { hero } from '../data';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  // destructure hero
+  const { title, subtitle, buttonText } = hero;
+  return (
+    <section className='h-[850px] w-full bg-hero bg-right bg-cover bg-no-repeat text-white'>
+      <div className='container mx-auto'>hero</div>
+    </section>
+  );
 };
 
 export default Hero;
